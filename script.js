c6fcb71b6ff5b2892185ece080e8d75538fd463f
wwcarlos <carmorenoc@unal.edu.co>
2025-05-16 15:51:56 -0500

    fix se intentó ocultar los elementos pero cuando tenia la idea clara se acabo el tiempo

--- script.js
+++ script.js
@@ -2,20 +2,16 @@ function next() {
     var vehiculo = document.getElementById("myselect")
     var formulario2 = document.getElementById("form2")
 
-    if (vehiculo.value === (0)) {
+    if (vehiculo.value === ("0")) {
 
     }
     if (vehiculo.value === ("carro")) {
         document.getElementById("days").placeholder = '¿cuantos dias guardará su carro?';
-        formulario2.remove("d-none");
     }
     if (vehiculo.value === ("moto")) {
         document.getElementById("days").placeholder = '¿cuantos dias guardará su moto?';
-        formulario2.remove("d-none");
     }
-    else {
-        formulario2.classList = ("d-none");
-    }
+
 }
 function multiplicar() {
     var dias = document.getElementById("days").value
